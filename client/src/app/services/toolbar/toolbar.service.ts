e0ed8d0b9e922d418b27f4987a366c53f980fea8
Yanis Toubal <yanis.toubal@polymtl.ca>
2020-09-13 18:54:15 -0400

    moved tools from drawing component to new service

---
+++ client/src/app/services/toolbar/toolbar.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Tool } from '@app/classes/tool';
+import { PencilService } from '../tools/pencil-service';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class ToolbarService {
+    private tools: Tool[];
+    currentTool: Tool;
+
+    constructor(pencilService: PencilService) {
+        this.tools = [pencilService];
+        this.currentTool = this.tools[0];
+    }
+}
