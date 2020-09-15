c17d355c8a8172b4d87905d8a7ac95c32b342c57
Yanis Toubal <yanis.toubal@polymtl.ca>
2020-09-15 00:09:26 -0400

    remove unnecessary access modifier

--- client/src/app/classes/tool.ts
+++ client/src/app/classes/tool.ts
@@ -6,7 +6,7 @@ import { Vec2 } from './vec2';
 export abstract class Tool {
     mouseDownCoord: Vec2;
     mouseDown: boolean = false;
-    protected name: string;
+    name: string;
     tooltip: string;
     iconName: string;
 
