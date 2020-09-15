7840bc9e1988d11eefa90707d38ce6aa5325b3ce
Yanis Toubal <yanis.toubal@polymtl.ca>
2020-09-15 18:55:49 -0400

    changing colors and spacing of home page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserModule } from '@angular/platform-browser';
@@ -13,7 +15,16 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
     declarations: [AppComponent, EditorComponent, SidebarComponent, DrawingComponent, MainPageComponent],
-    imports: [BrowserModule, HttpClientModule, AppRoutingModule, MatIconModule, MatTooltipModule, BrowserAnimationsModule],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        AppRoutingModule,
+        MatIconModule,
+        MatTooltipModule,
+        MatButtonModule,
+        MatDividerModule,
+        BrowserAnimationsModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
