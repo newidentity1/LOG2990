674b21c803906ae8b0b6fb2a755310b7a9a28ecc
Yanis Toubal <yanis.toubal@polymtl.ca>
2020-09-15 22:41:36 -0400

    fixing lint in the new files

--- client/src/app/components/tools-options/pencil/pencil.component.spec.ts
+++ client/src/app/components/tools-options/pencil/pencil.component.spec.ts
@@ -1,25 +1,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PencilComponent } from './pencil.component';
 
 describe('PencilComponent', () => {
-  let component: PencilComponent;
-  let fixture: ComponentFixture<PencilComponent>;
+    let component: PencilComponent;
+    let fixture: ComponentFixture<PencilComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ PencilComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [PencilComponent],
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(PencilComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(PencilComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
