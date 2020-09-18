46606becd0ead2ab7e197572b3796efaf22ca436
Yanis Toubal <yanis.toubal@polymtl.ca>
2020-09-18 14:44:22 -0400

    implementing toolProperties property and methods to apply the properties selected

--- client/src/app/services/tools/rectangle/rectangle.service.ts
+++ client/src/app/services/tools/rectangle/rectangle.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Tool } from '@app/classes/tool';
+import { BasicShapeProperties } from '@app/classes/tools-properties/basic-shape-properties';
 import { Vec2 } from '@app/classes/vec2';
 import { DrawingService } from '@app/services/drawing/drawing.service';
 
@@ -27,6 +28,7 @@ export class RectangleService extends Tool {
         this.name = 'Rectangle';
         this.tooltip = 'Rectangle';
         this.iconName = 'crop_square';
+        this.toolProperties = new BasicShapeProperties();
     }
 
     onMouseDown(event: MouseEvent): void {
@@ -48,7 +50,7 @@ export class RectangleService extends Tool {
                 this.height = square.y;
             }
             this.drawingService.clearCanvas(this.drawingService.previewCtx);
-            this.drawFillRect(this.drawingService.baseCtx, this.width, this.height);
+            this.draw(this.drawingService.baseCtx);
         }
         this.mouseDown = false;
     }
@@ -59,36 +61,43 @@ export class RectangleService extends Tool {
             this.computeDimensions(mousePosition);
 
             // On dessine sur le canvas de prévisualisation et on l'efface à chaque déplacement de la souris
-            this.drawingService.clearCanvas(this.drawingService.previewCtx);
-            this.drawPreview();
+            const previewCtx = this.drawingService.previewCtx;
+            this.drawingService.clearCanvas(previewCtx);
+            this.draw(previewCtx);
         }
     }
 
     onKeyDown(event: KeyboardEvent): void {
         if (event.key === 'Shift') {
             this.shiftDown = true;
-            this.drawPreview();
+            this.draw(this.drawingService.previewCtx);
         }
     }
 
     onKeyUp(event: KeyboardEvent): void {
         if (event.key === 'Shift') {
             this.shiftDown = false;
-            this.drawPreview();
+            this.draw(this.drawingService.previewCtx);
         }
     }
 
-    private drawPreview(): void {
-        let previewWidth = this.width;
-        let previewHeight = this.height;
+    private draw(ctx: CanvasRenderingContext2D): void {
+        let width = this.width;
+        let height = this.height;
         if (this.shiftDown) {
-            const square: Vec2 = this.transformToSquare(previewWidth, previewHeight);
-            previewWidth = square.x;
-            previewHeight = square.y;
+            const square: Vec2 = this.transformToSquare(width, height);
+            width = square.x;
+            height = square.y;
         }
         this.drawingService.clearCanvas(this.drawingService.previewCtx);
-        this.drawFillRect(this.drawingService.previewCtx, previewWidth, previewHeight);
-        this.drawStrokeRect(this.drawingService.previewCtx, previewWidth, previewHeight);
+        const rectangleProperties = this.toolProperties as BasicShapeProperties;
+        if (rectangleProperties.currentType === 'Plein') {
+            this.drawFillRect(ctx, width, height);
+        } else if (rectangleProperties.currentType === 'Contour') {
+            this.drawStrokeRect(ctx, width, height);
+        } else {
+            this.drawFillStrokeRect(ctx, width, height);
+        }
     }
 
     transformToSquare(width: number, height: number): Vec2 {
@@ -122,11 +131,28 @@ export class RectangleService extends Tool {
         return Math.abs(this.width) < Math.abs(this.height);
     }
 
+    setThickness(value: number | null): void {
+        value = value === null ? 1 : value;
+        this.toolProperties.thickness = value;
+        this.drawingService.previewCtx.lineWidth = value;
+        this.drawingService.baseCtx.lineWidth = value;
+    }
+
+    setTypeDrawing(value: string): void {
+        const rectangleProperties = this.toolProperties as BasicShapeProperties;
+        rectangleProperties.currentType = value;
+    }
+
     private drawFillRect(ctx: CanvasRenderingContext2D, width: number, height: number): void {
         ctx.fillRect(this.startingX, this.startingY, width, height);
     }
 
     private drawStrokeRect(ctx: CanvasRenderingContext2D, width: number, height: number): void {
         ctx.strokeRect(this.startingX, this.startingY, width, height);
+    }
+
+    private drawFillStrokeRect(ctx: CanvasRenderingContext2D, width: number, height: number): void {
+        this.drawFillRect(ctx, width, height);
+        this.drawStrokeRect(ctx, width, height);
     }
 }
