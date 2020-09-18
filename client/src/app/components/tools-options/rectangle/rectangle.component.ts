a1184b14f3ded02dde7a008e29bfac3116639841
Yanis Toubal <yanis.toubal@polymtl.ca>
2020-09-18 14:42:36 -0400

    creating event listener and event handlers for properties change in ui

--- client/src/app/components/tools-options/rectangle/rectangle.component.ts
+++ client/src/app/components/tools-options/rectangle/rectangle.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatRadioChange } from '@angular/material/radio';
+import { MatSliderChange } from '@angular/material/slider';
+import { RectangleService } from '@app/services/tools/rectangle/rectangle.service';
 
 @Component({
     selector: 'app-rectangle-options',
@@ -9,11 +12,19 @@ export class RectangleComponent implements OnInit {
     typesDrawing: string[] = ['Contour', 'Plein', 'Plein avec contour'];
     currentType: string = this.typesDrawing[0];
 
-    constructor() {
+    constructor(public rectangleService: RectangleService) {
         // TODO à  implementer
     }
 
     ngOnInit(): void {
         // TODO à implementer
+    }
+
+    onThicknessChange(event: MatSliderChange): void {
+        this.rectangleService.setThickness(event.value);
+    }
+
+    onTypeDrawingChange(event: MatRadioChange): void {
+        this.rectangleService.setTypeDrawing(event.value);
     }
 }
