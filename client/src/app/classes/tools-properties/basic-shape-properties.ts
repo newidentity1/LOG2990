56713675dafa4c8b13af8600c14291975bef5d32
Yanis Toubal <yanis.toubal@polymtl.ca>
2020-09-18 23:51:58 -0400

    added resetProperties method in basic-tool-properties and using it after changing tool

--- client/src/app/classes/tools-properties/basic-shape-properties.ts
+++ client/src/app/classes/tools-properties/basic-shape-properties.ts
@@ -1,8 +1,13 @@
 import { BasicToolProperties } from './basic-tool-properties';
 
-export class BasicShapeProperties implements BasicToolProperties {
+export class BasicShapeProperties extends BasicToolProperties {
     thickness: number = 1;
     // TODO créer un enum pour les types possibles
     typesDrawing: ['Contour', 'Plein', 'Plein avec contour'];
     currentType: string = 'Contour';
+
+    resetProperties(): void {
+        this.thickness = 1;
+        this.currentType = 'Contour';
+    }
 }
