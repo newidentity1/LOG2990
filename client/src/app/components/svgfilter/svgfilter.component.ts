b0376e77c217f0381c1bb9a4b231afb459c8d260
Estefan Vega-Calcada <estefan.vega-calcada@polymtl.ca>
2020-09-19 21:22:08 -0400

    Moved svg filters into a component

---
+++ client/src/app/components/svgfilter/svgfilter.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'svgfilter',
+    templateUrl: './svgfilter.component.html',
+    styleUrls: ['./svgfilter.component.scss'],
+})
+export class SVGFilterComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
