2ac609c64ce97f0aad18157db5c00f426f0b8f28
Yanis Toubal <yanis.toubal@polymtl.ca>
2020-09-25 19:43:21 -0400

    finished tests for rectangle.component

--- client/src/app/components/tools-options/rectangle/rectangle.component.spec.ts
+++ client/src/app/components/tools-options/rectangle/rectangle.component.spec.ts
@@ -1,19 +1,33 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSliderModule } from '@angular/material/slider';
+import { MatRadioChange, MatRadioModule, _MatRadioButtonBase } from '@angular/material/radio';
+import { MatSlider, MatSliderChange, MatSliderModule } from '@angular/material/slider';
 import { ThicknessSliderComponent } from '@app/components/tools-options/thickness-slider/thickness-slider.component';
+import { MAXIMUM_THICKNESS, MINIMUM_THICKNESS } from '@app/constants/constants';
+import { DrawingType } from '@app/enums/drawing-type.enum';
+import { RectangleService } from '@app/services/tools/rectangle/rectangle.service';
 import { RectangleComponent } from './rectangle.component';
 
 describe('RectangleComponent', () => {
     let component: RectangleComponent;
     let fixture: ComponentFixture<RectangleComponent>;
+    let rectangleService: RectangleService;
+    // tslint:disable-next-line: no-any / reason: spy of functions
+    let thicknessSpy: jasmine.SpyObj<any>;
+    let typeDrawingSpy: jasmine.SpyObj<any>;
+    let matSliderEvent: MatSliderChange;
+    // tslint:disable:prefer-const / reason: needed for matSliderSource and matRadioSource as placeholders
+    let matSliderSource: MatSlider;
+    let matRadioEvent: MatRadioChange;
+    let matRadioSource: _MatRadioButtonBase;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [RectangleComponent, ThicknessSliderComponent],
             imports: [MatSliderModule, MatRadioModule, FormsModule],
+            providers: [RectangleService],
         }).compileComponents();
+        rectangleService = TestBed.inject(RectangleService);
     }));
 
     beforeEach(() => {
@@ -24,5 +38,34 @@ describe('RectangleComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('onThicknessChange should call setThickness of rectangle service if value is inside scope', () => {
+        thicknessSpy = spyOn(rectangleService, 'setThickness').and.callThrough();
+        matSliderEvent = { source: matSliderSource, value: MAXIMUM_THICKNESS / 2 };
+        component.onThicknessChange(matSliderEvent);
+        // tslint:disable-next-line: no-string-literal
+        expect(thicknessSpy).toHaveBeenCalled();
+    });
+
+    it('onThicknessChange should not call setThickness of rectangle service if value is outside scope', () => {
+        thicknessSpy = spyOn(rectangleService, 'setThickness').and.callThrough();
+        matSliderEvent = { source: matSliderSource, value: MINIMUM_THICKNESS - 1 };
+        component.onThicknessChange(matSliderEvent);
+        expect(thicknessSpy).not.toHaveBeenCalled();
+    });
+
+    it('onTypeDrawingChange should call setTypeDrawing of rectangle service if value is in Enum DrawingType', () => {
+        typeDrawingSpy = spyOn(rectangleService, 'setTypeDrawing').and.callThrough();
+        matRadioEvent = { source: matRadioSource, value: DrawingType.Fill };
+        component.onTypeDrawingChange(matRadioEvent);
+        expect(typeDrawingSpy).toHaveBeenCalled();
+    });
+
+    it('onTypeDrawingChange should not call setTypeDrawing of rectangle service if value is not in Enum DrawingType', () => {
+        typeDrawingSpy = spyOn(rectangleService, 'setTypeDrawing').and.callThrough();
+        matRadioEvent = { source: matRadioSource, value: '' };
+        component.onTypeDrawingChange(matRadioEvent);
+        expect(typeDrawingSpy).not.toHaveBeenCalled();
     });
 });
