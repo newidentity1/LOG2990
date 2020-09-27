aa10d631eb6dc59f91bef2dcda39f484d191b661
momoislam <mohammed-ariful.islam@polymtl.ca>
2020-09-27 18:53:47 -0400

    added tests for pencil component and service

--- client/src/app/components/tools-options/pencil/pencil.component.spec.ts
+++ client/src/app/components/tools-options/pencil/pencil.component.spec.ts
@@ -1,12 +1,18 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatSliderModule } from '@angular/material/slider';
+import { MatSliderChange, MatSliderModule } from '@angular/material/slider';
 import { ThicknessSliderComponent } from '@app/components/tools-options/thickness-slider/thickness-slider.component';
+import { MINIMUM_THICKNESS } from '@app/constants/constants';
+import { PencilService } from '@app/services/tools/pencil/pencil-service';
 import { PencilComponent } from './pencil.component';
 
 describe('PencilComponent', () => {
     let component: PencilComponent;
     let fixture: ComponentFixture<PencilComponent>;
+    let pencilServiceStub: PencilService;
+    // tslint:disable: no-any / reason: spy of functions
+    let thicknessSpy: jasmine.SpyObj<any>;
+    // tslint:enable: no-any
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -14,6 +20,9 @@ describe('PencilComponent', () => {
             imports: [MatSliderModule],
             schemas: [CUSTOM_ELEMENTS_SCHEMA],
         }).compileComponents();
+        pencilServiceStub = TestBed.inject(PencilService);
+
+        thicknessSpy = spyOn(pencilServiceStub, 'setThickness').and.callThrough();
     }));
 
     beforeEach(() => {
@@ -24,5 +33,10 @@ describe('PencilComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should create', () => {
+        component.onThicknessChange({ value: MINIMUM_THICKNESS } as MatSliderChange);
+        expect(thicknessSpy).toHaveBeenCalled();
     });
 });
