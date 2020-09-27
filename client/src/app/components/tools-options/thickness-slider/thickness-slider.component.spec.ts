bcdbf880092c5f5e6fde7453f9c2b31fc93ef442
Yanis Toubal <yanis.toubal@polymtl.ca>
2020-09-27 18:14:27 -0400

    thickness slider tests done

--- client/src/app/components/tools-options/thickness-slider/thickness-slider.component.spec.ts
+++ client/src/app/components/tools-options/thickness-slider/thickness-slider.component.spec.ts
@@ -1,11 +1,14 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatSliderModule } from '@angular/material/slider';
+import { MatSlider, MatSliderChange, MatSliderModule } from '@angular/material/slider';
+import { MAXIMUM_THICKNESS, MINIMUM_THICKNESS } from '@app/constants/constants';
 import { ThicknessSliderComponent } from './thickness-slider.component';
 
 describe('ThicknessSliderComponent', () => {
     let component: ThicknessSliderComponent;
     let fixture: ComponentFixture<ThicknessSliderComponent>;
+    // tslint:disable-next-line: prefer-const
+    let sliderSource: MatSlider;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -23,5 +26,54 @@ describe('ThicknessSliderComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('onThicknessChange should emit if the thickness value is MINIMUM_THICKNESS', () => {
+        const thicknessValue = MINIMUM_THICKNESS;
+        const sliderChange: MatSliderChange = { source: sliderSource, value: thicknessValue };
+        const emitSpy = spyOn(component.thicknessChange, 'emit');
+        component.onThicknessChange(sliderChange);
+        expect(emitSpy).toHaveBeenCalled();
+        expect(emitSpy).toHaveBeenCalledWith(sliderChange);
+    });
+
+    it('onThicknessChange should not emit if the thickness value is less than MININIM_THICKNESS', () => {
+        const thicknessValue = MINIMUM_THICKNESS - 1;
+        const sliderChange: MatSliderChange = { source: sliderSource, value: thicknessValue };
+        const emitSpy = spyOn(component.thicknessChange, 'emit');
+        component.onThicknessChange(sliderChange);
+        expect(emitSpy).not.toHaveBeenCalled();
+    });
+
+    it('onThicknessChange should emit if the thickness value is MAXIMUM_THICKNESS', () => {
+        const thicknessValue = MAXIMUM_THICKNESS;
+        const sliderChange: MatSliderChange = { source: sliderSource, value: thicknessValue };
+        const emitSpy = spyOn(component.thicknessChange, 'emit');
+        component.onThicknessChange(sliderChange);
+        expect(emitSpy).toHaveBeenCalled();
+        expect(emitSpy).toHaveBeenCalledWith(sliderChange);
+    });
+
+    it('onThicknessChange should not emit if the thickness value is greather than MAXIMUM_THICKNESS', () => {
+        const thicknessValue = MAXIMUM_THICKNESS + 1;
+        const sliderChange: MatSliderChange = { source: sliderSource, value: thicknessValue };
+        const emitSpy = spyOn(component.thicknessChange, 'emit');
+        component.onThicknessChange(sliderChange);
+        expect(emitSpy).not.toHaveBeenCalled();
+    });
+    it('minimumThickness should return the minimum thickness contant value', () => {
+        const value = component.minimumThickness;
+        const expectedResult = MINIMUM_THICKNESS;
+        expect(value).toEqual(expectedResult);
+    });
+
+    it('maximumThickness should return the maximum thickness contant value', () => {
+        const value = component.maximumThickness;
+        const expectedResult = MAXIMUM_THICKNESS;
+        expect(value).toEqual(expectedResult);
     });
 });
