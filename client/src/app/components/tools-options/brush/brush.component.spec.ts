833493319057cde3c8bc8c2ea9d990f21d104204
Yanis Toubal <yanis.toubal@polymtl.ca>
2020-09-27 18:58:55 -0400

    fix brush tests

--- client/src/app/components/tools-options/brush/brush.component.spec.ts
+++ client/src/app/components/tools-options/brush/brush.component.spec.ts
@@ -1,5 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSliderModule } from '@angular/material/slider';
+import { ThicknessSliderComponent } from '@app/components/tools-options/thickness-slider/thickness-slider.component';
 import { BrushComponent } from './brush.component';
 
 describe('BrushComponent', () => {
@@ -8,7 +11,8 @@ describe('BrushComponent', () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [BrushComponent],
+            declarations: [BrushComponent, ThicknessSliderComponent],
+            imports: [MatRadioModule, MatSliderModule, FormsModule],
         }).compileComponents();
     }));
 
