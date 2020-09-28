6bd4457059393cb91a7260dab0fbcde5f1faf9a7
Yanis Toubal <yanis.toubal@polymtl.ca>
2020-09-27 21:03:52 -0400

    sidebar component tests done

--- client/src/app/components/sidebar/sidebar.component.spec.ts
+++ client/src/app/components/sidebar/sidebar.component.spec.ts
@@ -12,11 +12,13 @@ import { PencilComponent } from '@app/components/tools-options/pencil/pencil.com
 import { RectangleComponent } from '@app/components/tools-options/rectangle/rectangle.component';
 import { ThicknessSliderComponent } from '@app/components/tools-options/thickness-slider/thickness-slider.component';
 import { RecentColorsComponent } from '@app/recent-colors/recent-colors.component';
+import { ToolbarService } from '@app/services/toolbar/toolbar.service';
 import { SidebarComponent } from './sidebar.component';
 
 describe('SidebarComponent', () => {
     let component: SidebarComponent;
     let fixture: ComponentFixture<SidebarComponent>;
+    let toolbarService: ToolbarService;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -30,9 +32,11 @@ describe('SidebarComponent', () => {
                 ThicknessSliderComponent,
             ],
             imports: [MatIconModule, MatTooltipModule, MatSidenavModule, BrowserAnimationsModule, MatSliderModule],
-            providers: [{ provide: MatDialog, useValue: {} }],
+            providers: [{ provide: MatDialog, useValue: {} }, ToolbarService],
             schemas: [CUSTOM_ELEMENTS_SCHEMA],
         }).compileComponents();
+
+        toolbarService = TestBed.inject(ToolbarService);
     }));
 
     beforeEach(() => {
@@ -43,5 +47,55 @@ describe('SidebarComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('tools should be the same as toolbarService tools', () => {
+        const expectedResult = toolbarService.getTools();
+        expect(component.tools).toEqual(expectedResult);
+    });
+
+    it('isCurrentTool should return true if the parameter is the currentTool', () => {
+        toolbarService.currentTool = component.tools[0];
+        const tool = toolbarService.currentTool;
+        const result = component.isCurrentTool(tool);
+        expect(result).toEqual(true);
+    });
+
+    it('isCurrentTool should return false if the parameter is not the currentTool', () => {
+        toolbarService.currentTool = component.tools[0];
+        const tool = component.tools[1];
+        const result = component.isCurrentTool(tool);
+        expect(result).toEqual(false);
+    });
+
+    it('onToolChanged should change the currentTool, call applyCurrentToolColor of toolbarService and open the MatSideNav if the parameter is not the currentTool', () => {
+        toolbarService.currentTool = component.tools[0];
+        const tool = component.tools[1];
+        const spyApplyCurrent = spyOn(toolbarService, 'applyCurrentToolColor');
+        const spySideNav = spyOn(component.sidenavProperties, 'open');
+        component.onToolChanged(tool);
+        expect(toolbarService.currentTool).toEqual(tool);
+        expect(spyApplyCurrent).toHaveBeenCalled();
+        expect(spySideNav).toHaveBeenCalled();
+    });
+
+    it('onToolChanged should toggle the MatSideNav if the parameter is not the currentTool', () => {
+        toolbarService.currentTool = component.tools[0];
+        const tool = component.tools[0];
+        const spySideNav = spyOn(component.sidenavProperties, 'toggle');
+        component.onToolChanged(tool);
+        expect(spySideNav).toHaveBeenCalled();
+    });
+
+    it('get currentTool should return the current tool of toolbarService', () => {
+        toolbarService.currentTool = component.tools[0];
+        const currentTool = toolbarService.currentTool;
+        expect(component.currentTool).toEqual(currentTool);
+    });
+
+    it('set currentTool should set the current tool of toolbarService', () => {
+        const currentTool = component.tools[0];
+        component.currentTool = currentTool;
+        expect(toolbarService.currentTool).toEqual(currentTool);
     });
 });
