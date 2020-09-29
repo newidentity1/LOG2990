928c80b6863d3f88ddc2627342f688cf9598dc93
momoislam <mohammed-ariful.islam@polymtl.ca>
2020-09-29 19:41:24 -0400

    added tests for pencil component

--- client/src/app/components/tools-options/pencil/pencil.component.spec.ts
+++ client/src/app/components/tools-options/pencil/pencil.component.spec.ts
@@ -2,17 +2,17 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatSliderChange, MatSliderModule } from '@angular/material/slider';
 import { ThicknessSliderComponent } from '@app/components/tools-options/thickness-slider/thickness-slider.component';
-import { MINIMUM_THICKNESS } from '@app/constants/constants';
+import { MAXIMUM_THICKNESS, MINIMUM_THICKNESS } from '@app/constants/constants';
 import { PencilService } from '@app/services/tools/pencil/pencil-service';
 import { PencilComponent } from './pencil.component';
 
 describe('PencilComponent', () => {
     let component: PencilComponent;
     let fixture: ComponentFixture<PencilComponent>;
-    let pencilServiceStub: PencilService;
-    // tslint:disable: no-any / reason: spy of functions
+    let pencilServiceMock: jasmine.SpyObj<PencilService>;
+
+    // tslint:disable-next-line: no-any / reason: spy of functions
     let thicknessSpy: jasmine.SpyObj<any>;
-    // tslint:enable: no-any
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -20,9 +20,9 @@ describe('PencilComponent', () => {
             imports: [MatSliderModule],
             schemas: [CUSTOM_ELEMENTS_SCHEMA],
         }).compileComponents();
-        pencilServiceStub = TestBed.inject(PencilService);
 
-        thicknessSpy = spyOn(pencilServiceStub, 'setThickness').and.callThrough();
+        pencilServiceMock = TestBed.inject(PencilService) as jasmine.SpyObj<PencilService>;
+        thicknessSpy = spyOn(pencilServiceMock, 'setThickness').and.callThrough();
     }));
 
     beforeEach(() => {
@@ -35,8 +35,15 @@ describe('PencilComponent', () => {
         expect(component).toBeTruthy();
     });
 
-    it('should create', () => {
-        component.onThicknessChange({ value: MINIMUM_THICKNESS } as MatSliderChange);
-        expect(thicknessSpy).toHaveBeenCalled();
+    it('onThicknessChange should call setThickness of pencilService if value is inside scope', () => {
+        const matSliderEvent = { value: MAXIMUM_THICKNESS / 2 } as MatSliderChange;
+        component.onThicknessChange(matSliderEvent);
+        expect(thicknessSpy).toHaveBeenCalledWith(MAXIMUM_THICKNESS / 2);
+    });
+
+    it('onThicknessChange should not call setThickness of pencilService if value is outside scope', () => {
+        const matSliderEvent = { value: MINIMUM_THICKNESS - 1 } as MatSliderChange;
+        component.onThicknessChange(matSliderEvent);
+        expect(thicknessSpy).not.toHaveBeenCalledWith(matSliderEvent.value);
     });
 });
