c0b0e4d71dd945aa37c0d8a00361238d6d31ca7e
Brando Tovar <grover-brando.tovar-oblitas@polymtl.ca>
2020-09-30 01:12:03 -0400

    tests ellipse component

--- client/src/app/components/tools-options/ellipse/ellipse.component.spec.ts
+++ client/src/app/components/tools-options/ellipse/ellipse.component.spec.ts
@@ -1,20 +1,36 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSliderModule } from '@angular/material/slider';
+import { MatRadioChange, MatRadioModule, _MatRadioButtonBase } from '@angular/material/radio';
+import { MatSlider, MatSliderChange, MatSliderModule } from '@angular/material/slider';
+import { BasicShapeProperties } from '@app/classes/tools-properties/basic-shape-properties';
+import { MAXIMUM_THICKNESS, MINIMUM_THICKNESS } from '@app/constants/constants';
+import { DrawingType } from '@app/enums/drawing-type.enum';
+import { EllipseService } from '@app/services/tools/ellipse/ellipse.service';
+import { ThicknessSliderComponent } from '../thickness-slider/thickness-slider.component';
 import { EllipseComponent } from './ellipse.component';
 
 describe('EllipseComponent', () => {
     let component: EllipseComponent;
     let fixture: ComponentFixture<EllipseComponent>;
+    let ellipseServiceMock: jasmine.SpyObj<EllipseService>;
+    let matSliderEvent: MatSliderChange;
+    // tslint:disable-next-line: prefer-const / reason: needed for matSliderSource and matRadioSource as placeholders
+    let matSliderSource: MatSlider;
+    let matRadioEvent: MatRadioChange;
+    // tslint:disable-next-line: prefer-const
+    let matRadioSource: _MatRadioButtonBase;
 
     beforeEach(async(() => {
+        ellipseServiceMock = jasmine.createSpyObj('EllipseService', ['setThickness', 'setTypeDrawing']);
         TestBed.configureTestingModule({
-            declarations: [EllipseComponent],
+            declarations: [EllipseComponent, ThicknessSliderComponent],
             imports: [MatRadioModule, MatSliderModule, FormsModule],
+            providers: [{ provide: EllipseService, useValue: ellipseServiceMock }],
             schemas: [CUSTOM_ELEMENTS_SCHEMA],
         }).compileComponents();
+        ellipseServiceMock = TestBed.inject(EllipseService) as jasmine.SpyObj<EllipseService>;
+        ellipseServiceMock.toolProperties = new BasicShapeProperties();
     }));
 
     beforeEach(() => {
@@ -25,5 +41,31 @@ describe('EllipseComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('onThicknessChange should call setThickness of rectangle service if value is inside scope', () => {
+        matSliderEvent = { source: matSliderSource, value: MAXIMUM_THICKNESS / 2 };
+        component.onThicknessChange(matSliderEvent);
+        expect(ellipseServiceMock.setThickness).toHaveBeenCalled();
+        expect(ellipseServiceMock.setThickness).toHaveBeenCalledWith(MAXIMUM_THICKNESS / 2);
+    });
+
+    it('onThicknessChange should not call setThickness of ellipse service if value is outside scope', () => {
+        matSliderEvent = { source: matSliderSource, value: MINIMUM_THICKNESS - 1 };
+        component.onThicknessChange(matSliderEvent);
+        expect(ellipseServiceMock.setThickness).not.toHaveBeenCalledWith(matSliderEvent.value);
+    });
+
+    it('onTypeDrawingChange should call setTypeDrawing of rectangle service if value is in Enum DrawingType', () => {
+        matRadioEvent = { source: matRadioSource, value: DrawingType.Fill };
+        component.onTypeDrawingChange(matRadioEvent);
+        expect(ellipseServiceMock.setTypeDrawing).toHaveBeenCalled();
+        expect(ellipseServiceMock.setTypeDrawing).toHaveBeenCalledWith(DrawingType.Fill);
+    });
+
+    it('onTypeDrawingChange should not call setTypeDrawing of rectangle service if value is not in Enum DrawingType', () => {
+        matRadioEvent = { source: matRadioSource, value: '' };
+        component.onTypeDrawingChange(matRadioEvent);
+        expect(ellipseServiceMock.setTypeDrawing).not.toHaveBeenCalled();
     });
 });
