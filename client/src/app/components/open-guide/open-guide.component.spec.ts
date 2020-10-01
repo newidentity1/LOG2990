d1b40f8368f0672ccc4215b74483b5293257dc52
Brando Tovar <grover-brando.tovar-oblitas@polymtl.ca>
2020-10-01 00:29:04 -0400

    tested guidecomponent

--- client/src/app/components/open-guide/open-guide.component.spec.ts
+++ client/src/app/components/open-guide/open-guide.component.spec.ts
@@ -1,15 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialog } from '@angular/material/dialog';
 
 import { OpenGuideComponent } from './open-guide.component';
+
+class MatDialogMock {
+    // When the component calls this.dialog.open(...) we'll return an empty object
+    open(): {} {
+        return {};
+    }
+}
 
 describe('OpenGuideComponent', () => {
     let component: OpenGuideComponent;
     let fixture: ComponentFixture<OpenGuideComponent>;
+    // tslint:disable:no-any / reason: jasmine spy
+    let dialogOpenSpy: jasmine.Spy<any>;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [OpenGuideComponent],
+            providers: [{ provide: MatDialog, useClass: MatDialogMock }],
         }).compileComponents();
+        dialogOpenSpy = spyOn<any>(TestBed.inject(MatDialog), 'open').and.callThrough();
     }));
 
     beforeEach(() => {
@@ -21,4 +33,22 @@ describe('OpenGuideComponent', () => {
     it('should create', () => {
         expect(component).toBeTruthy();
     });
+
+    it('openPrimaryColorPicker should open dialog from menu', async(() => {
+        component.inMenu = true;
+        const button = fixture.debugElement.nativeElement.querySelector('button');
+        button.click();
+        fixture.whenStable().then(() => {
+            expect(dialogOpenSpy).toHaveBeenCalled();
+        });
+    }));
+
+    it('openPrimaryColorPicker should open dialog from drawing view', async(() => {
+        component.inMenu = true;
+        const button = fixture.debugElement.nativeElement.querySelector('button');
+        button.click();
+        fixture.whenStable().then(() => {
+            expect(dialogOpenSpy).toHaveBeenCalled();
+        });
+    }));
 });
