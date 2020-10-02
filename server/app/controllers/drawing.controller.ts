4d1520c3eac65b3da36fddceeb9f09d649cc6f63
Yanis Toubal <yanis.toubal@polymtl.ca>
2020-10-02 19:10:32 -0400

    finished drawing api with get, getAll, addOne, deleteOne and added new class Drawing for communication

--- server/app/controllers/drawing.controller.ts
+++ server/app/controllers/drawing.controller.ts
@@ -1,4 +1,5 @@
 import { DrawingService } from '@app/services/drawing.service';
+import { HTTP_STATUS_BAD_REQUEST, HTTP_STATUS_CREATED, HTTP_STATUS_OK } from 'app/contants';
 import { NextFunction, Request, Response, Router } from 'express';
 import { inject, injectable } from 'inversify';
 import { TYPES } from '../types';
@@ -14,9 +15,41 @@ export class DrawingController {
     private configureRouter(): void {
         this.router = Router();
 
-        this.router.get('/', (req: Request, res: Response, next: NextFunction) => {
-            // TODO
-            this.drawingService.test();
+        this.router.get('/:id', async (req: Request, res: Response, next: NextFunction) => {
+            const drawing = await this.drawingService.getDrawing(req.params.id);
+            res.json(drawing);
+        });
+
+        this.router.get('/', async (req: Request, res: Response, next: NextFunction) => {
+            const drawing = await this.drawingService.getDrawings();
+            res.json(drawing);
+        });
+
+        this.router.post('/', async (req: Request, res: Response, next: NextFunction) => {
+            const drawing = await this.drawingService
+                .addDrawing(req.body)
+                .then(() => {
+                    res.sendStatus(HTTP_STATUS_CREATED);
+                })
+                .catch((error) => {
+                    res.sendStatus(HTTP_STATUS_BAD_REQUEST).send(error);
+                });
+
+            res.json(drawing);
+        });
+
+        this.router.delete('/:id', async (req: Request, res: Response, next: NextFunction) => {
+            console.log(req.params.id);
+            const drawing = await this.drawingService
+                .removeDrawing(req.params.id)
+                .then(() => {
+                    res.sendStatus(HTTP_STATUS_OK);
+                })
+                .catch((error) => {
+                    res.send(error);
+                });
+
+            res.json(drawing);
         });
     }
 }
--- server/app/services/drawing.service.ts
+++ server/app/services/drawing.service.ts
@@ -1,15 +1,19 @@
+import { Drawing } from '@common/communication/drawing.ts';
 import * as CONSTANTS from 'app/contants';
 import { injectable } from 'inversify';
-import { MongoClient } from 'mongodb';
+import { Collection, MongoClient, ObjectId } from 'mongodb';
 import 'reflect-metadata';
 
 @injectable()
 export class DrawingService {
+    collection: Collection<Drawing>;
+
     constructor() {
         console.log(CONSTANTS.DATABASE_URL);
         MongoClient.connect(CONSTANTS.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
             .then((client: MongoClient) => {
                 // TODO
+                this.collection = client.db(CONSTANTS.DATABASE_NAME).collection(CONSTANTS.DATABASE_COLLECTION);
             })
             .catch((error) => {
                 console.log(error);
@@ -18,7 +22,53 @@ export class DrawingService {
             });
     }
 
-    test(): string {
-        return 'hello';
+    async getDrawing(id: string): Promise<Drawing> {
+        return this.collection
+            .findOne({ _id: new ObjectId(id) })
+            .then((drawing: Drawing) => {
+                console.log(drawing);
+                return drawing;
+            })
+            .catch((error) => {
+                // TODO voir comment gérer les erreurs
+                console.log('erreur de getDrawing' + error);
+                return error;
+            });
+    }
+
+    async getDrawings(): Promise<Drawing> {
+        return this.collection
+            .find({})
+            .toArray()
+            .then((drawings: Drawing[]) => {
+                return drawings;
+            })
+            .catch((error) => {
+                // TODO voir comment gérer les erreurs
+                console.log('erreur de getDrawings' + error);
+                return error;
+            });
+    }
+
+    async addDrawing(drawing: Drawing): Promise<void> {
+        this.collection.insertOne(drawing).catch((error) => {
+            // TODO voir comment gérer les erreurs
+            console.log('erreur de addDrawing' + error);
+            return error;
+        });
+    }
+
+    async removeDrawing(id: string): Promise<void> {
+        console.log(new ObjectId(id));
+        this.collection
+            .findOneAndDelete({ _id: new ObjectId(id) })
+            .then(() => {
+                // do nothing
+            })
+            .catch((error) => {
+                // TODO voir comment gérer les erreurs
+                console.log('erreur de removeDrawing' + error);
+                return error;
+            });
     }
 }
