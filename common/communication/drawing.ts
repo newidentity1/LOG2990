1076520ae45e0c4ba0c4005f79ef993acec5c318
Yanis Toubal <yanis.toubal@polymtl.ca>
2020-10-02 19:10:32 -0400

    finished drawing api with get, getAll, addOne, deleteOne and added new class Drawing for communication

---
+++ common/communication/drawing.ts
@@ -0,0 +1,6 @@
+export interface Drawing {
+    id_: string;
+    name: string;
+    tags: string[];
+    url: string;
+}
--- server/app/contants.ts
+++ server/app/contants.ts
@@ -6,3 +6,8 @@ dotenv.config();
 export const DATABASE_URL: string = process.env.DATABASE_URL as string;
 export const DATABASE_NAME: string = process.env.DATABASE_NAME as string;
 export const DATABASE_COLLECTION: string = process.env.DATABASE_COLLECTION as string;
+
+// HTTP status
+export const HTTP_STATUS_OK = 200;
+export const HTTP_STATUS_CREATED = 201;
+export const HTTP_STATUS_BAD_REQUEST = 400;
