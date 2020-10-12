d233ff3694918c676bc8820d4ffcd27cdfb784f5
Yanis Toubal <yanis.toubal@polymtl.ca>
2020-10-11 22:02:16 -0400

    added missing unsubscribe in drawing component

--- client/src/app/components/drawing/drawing.component.ts
+++ client/src/app/components/drawing/drawing.component.ts
@@ -1,17 +1,28 @@
-import { AfterContentInit, AfterViewInit, Component, ElementRef, EventEmitter, HostListener, Input, Output, ViewChild } from '@angular/core';
+import {
+    AfterContentInit,
+    AfterViewInit,
+    Component,
+    ElementRef,
+    EventEmitter,
+    HostListener,
+    Input,
+    OnDestroy,
+    Output,
+    ViewChild,
+} from '@angular/core';
 import { Vec2 } from '@app/classes/vec2';
 import { CANVAS_MARGIN_LEFT, CANVAS_MARGIN_TOP, CANVAS_MIN_HEIGHT, CANVAS_MIN_WIDTH, DEFAULT_HEIGHT, DEFAULT_WIDTH } from '@app/constants/constants';
 import { MouseButton } from '@app/enums/mouse-button.enum';
 import { DrawingService } from '@app/services/drawing/drawing.service';
 import { ToolbarService } from '@app/services/toolbar/toolbar.service';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-drawing',
     templateUrl: './drawing.component.html',
     styleUrls: ['./drawing.component.scss'],
 })
-export class DrawingComponent implements AfterViewInit, AfterContentInit {
+export class DrawingComponent implements AfterViewInit, AfterContentInit, OnDestroy {
     @ViewChild('baseCanvas', { static: false }) baseCanvas: ElementRef<HTMLCanvasElement>;
     // On utilise ce canvas pour dessiner sans affecter le dessin final
     @ViewChild('previewCanvas', { static: false }) previewCanvas: ElementRef<HTMLCanvasElement>;
@@ -25,7 +36,8 @@ export class DrawingComponent implements AfterViewInit, AfterContentInit {
     private baseCtx: CanvasRenderingContext2D;
     private previewCtx: CanvasRenderingContext2D;
     private canvasSize: Vec2 = { x: DEFAULT_WIDTH, y: DEFAULT_HEIGHT };
-
+    private subscribeCreateNewDrawing: Subscription;
+    private subscribeDimensionsUpdated: Subscription;
     isResizingWidth: boolean = false;
     isResizingHeight: boolean = false;
 
@@ -42,10 +54,10 @@ export class DrawingComponent implements AfterViewInit, AfterContentInit {
         this.drawingService.previewCtx = this.previewCtx;
         this.drawingService.canvas = this.baseCanvas.nativeElement;
 
-        this.drawingService.createNewDrawingEventListener().subscribe(() => {
+        this.subscribeCreateNewDrawing = this.drawingService.createNewDrawingEventListener().subscribe(() => {
             this.requestDrawingContainerDimensions.emit();
         });
-        this.dimensionsUpdatedEvent.subscribe((dimensions) => {
+        this.subscribeDimensionsUpdated = this.dimensionsUpdatedEvent.subscribe((dimensions) => {
             this.drawingContainerWidth = dimensions[0];
             this.drawingContainerHeight = dimensions[1];
             this.newCanvasSetSize();
@@ -54,6 +66,11 @@ export class DrawingComponent implements AfterViewInit, AfterContentInit {
             }, 0);
         });
         this.toolbarService.initializeColors();
+    }
+
+    ngOnDestroy(): void {
+        this.subscribeCreateNewDrawing.unsubscribe();
+        this.subscribeDimensionsUpdated.unsubscribe();
     }
 
     onMouseMove(event: MouseEvent): void {
