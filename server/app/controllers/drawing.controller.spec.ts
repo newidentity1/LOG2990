4027155408ff88edc4c457f5d4c2366b1c42015f
Yanis Toubal <yanis.toubal@polymtl.ca>
2020-10-14 17:50:10 -0400

    fixed constants path in drawing service tests

--- server/app/controllers/drawing.controller.spec.ts
+++ server/app/controllers/drawing.controller.spec.ts
@@ -1,7 +1,7 @@
 import { Application } from '@app/app';
 import { DrawingService } from '@app/services/drawing.service';
 import { TYPES } from '@app/types';
-import { HTTP_STATUS_BAD_REQUEST, HTTP_STATUS_CREATED, HTTP_STATUS_NOT_FOUND, HTTP_STATUS_NO_CONTENT, HTTP_STATUS_OK } from 'app/contants';
+import { HTTP_STATUS_BAD_REQUEST, HTTP_STATUS_CREATED, HTTP_STATUS_NOT_FOUND, HTTP_STATUS_NO_CONTENT, HTTP_STATUS_OK } from 'app/constants';
 import { expect } from 'chai';
 import { describe } from 'mocha';
 import * as supertest from 'supertest';
