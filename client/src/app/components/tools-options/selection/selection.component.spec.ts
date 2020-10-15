10fdba9c7fecabe6de19ce759da0ce8be77935c7
momoislam <mohammed-ariful.islam@polymtl.ca>
2020-10-14 20:39:33 -0400

    added tests for selectino component

--- client/src/app/components/tools-options/selection/selection.component.spec.ts
+++ client/src/app/components/tools-options/selection/selection.component.spec.ts
@@ -1,13 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { SelectionType } from '@app/enums/selection-type.enum';
+import { SelectionService } from '@app/services/tools/selection/selection.service';
 import { SelectionComponent } from './selection.component';
 
 describe('SelectionComponent', () => {
     let component: SelectionComponent;
     let fixture: ComponentFixture<SelectionComponent>;
+    let selectionServiceMock: jasmine.SpyObj<SelectionService>;
 
     beforeEach(async(() => {
+        selectionServiceMock = jasmine.createSpyObj('SelectionService', ['setSelectionType', 'selectAll']);
+
         TestBed.configureTestingModule({
             declarations: [SelectionComponent],
+            providers: [{ provide: SelectionService, useValue: selectionServiceMock }],
         }).compileComponents();
     }));
 
@@ -19,5 +25,27 @@ describe('SelectionComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('onTypeSelectionChange should call setSelectionType of selection service', () => {
+        selectionServiceMock.currentType = SelectionType.RectangleSelection;
+        const expectedType = SelectionType.EllipseSelection;
+        component.onTypeSelectionChange(expectedType);
+        expect(selectionServiceMock.setSelectionType).toHaveBeenCalledWith(expectedType);
+    });
+
+    it('onTypeSelectionChange should call selectAll of selection service', () => {
+        component.triggerSelectAll();
+        expect(selectionServiceMock.selectAll).toHaveBeenCalledWith();
+    });
+
+    it('keepOriginalOrder should return 0', () => {
+        const result = component.keepOriginalOrder();
+        expect(result).toEqual(0);
+    });
+
+    it('currentType getter should return currentType of selection service', () => {
+        const currentType = component.currentType;
+        expect(currentType).toEqual(selectionServiceMock.currentType);
     });
 });
