20e3f283c1850c54a697cf21c44725a0e9b017fa
NoeBerguin <berguin.noe@gmail.com>
2020-10-18 12:09:25 -0400

    creat gallery component

---
+++ client/src/app/components/gallery/gallery.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+    selector: 'app-open-gallery',
+    templateUrl: './gallery.component.html',
+    styleUrls: ['./gallery.component.scss'],
+})
+export class GalleryComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
