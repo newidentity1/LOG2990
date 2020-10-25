b5e0928ececf688a50b517ea12e7f868a82c24f6
Yanis Toubal <yanis.toubal@polymtl.ca>
2020-10-24 21:50:07 -0400

    added download drawing functionality

--- client/src/app/services/export-drawing/export-drawing.service.ts
+++ client/src/app/services/export-drawing/export-drawing.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { DrawingService } from '@app/services/drawing/drawing.service';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class ExportDrawingService {
+    constructor(public drawingService: DrawingService) {}
 
-  constructor() { }
+    getCanvasImageUrl(): string {
+        return this.drawingService.canvas.toDataURL();
+    }
 }
