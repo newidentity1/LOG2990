243905b33a516fb1cde5c13018b6e321389b8080
Noe <noe.berguin@polymtl.ca>
2020-10-25 18:29:23 -0400

    export

---
+++ client/src/app/components/upload/upload.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { DrawingService } from '@app/services/drawing/drawing.service';
+
+@Component({
+    selector: 'app-upload',
+    templateUrl: './upload.component.html',
+    styleUrls: ['./upload.component.scss'],
+})
+export class UploadComponent implements OnInit {
+    filePath: String;
+    constructor(public drawingService: DrawingService, private afStorage: AngularFireStorage) {}
+    uploadImage(): void {
+        this.saveImage();
+        // console.log(this.filePath);
+        this.afStorage.upload('/images' + Math.random() + this.filePath, this.filePath);
+    }
+
+    saveImage(): void {
+        const myImage = this.drawingService.canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream');
+        // window.location.href = myImage;
+        const link = document.createElement('a');
+        link.download = 'image.png';
+        link.href = myImage;
+        link.click();
+        console.log(this.filePath);
+        // tslint:disable-next-line: prettier
+        this.filePath = 'C:\Users\Noe Berguin\Downloads'; 
+    }
+
+    ngOnInit(): void {
+        // TODO
+    }
+}
