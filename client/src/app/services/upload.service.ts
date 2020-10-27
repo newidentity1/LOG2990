9c651640757a96d4156637802bc1a1f5190727bc
Noe <noe.berguin@polymtl.ca>
2020-10-27 17:37:42 -0400

    need to continue post draw

--- client/src/app/services/upload.service.ts
+++ client/src/app/services/upload.service.ts
@@ -1,7 +1,8 @@
-// import { HttpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask } from '@angular/fire/storage';
 import { DrawingService } from '@app/services/drawing/drawing.service';
+import { Drawing } from '@common/communication/drawing';
 // import { Drawing } from '@common/communication/drawing';
 import { Observable } from 'rxjs';
 
@@ -12,15 +13,16 @@ export class UploadService {
     ref: AngularFireStorageReference;
     task: AngularFireUploadTask;
     url: string = '';
-    constructor(public drawingService: DrawingService, private afStorage: AngularFireStorage) {}
+    id: string = '';
+    constructor(public drawingService: DrawingService, private afStorage: AngularFireStorage, private http: HttpClient) {}
 
     uploadCanvas(): void {
         // met l'image dans la fireBase
-        const id = Math.random() + 'test';
+        this.id = Math.random() + 'test';
         const baseImage = new Image();
         baseImage.src = this.drawingService.canvas.toDataURL('image/png');
         this.drawingService.canvas.toBlob((blob) => {
-            this.ref = this.afStorage.ref(id);
+            this.ref = this.afStorage.ref(this.id);
             this.task = this.ref.put(blob);
             this.task.snapshotChanges().subscribe((event) => {
                 // ajouter ici le telechargement des donnees si besoin
@@ -38,11 +40,18 @@ export class UploadService {
                 this.url = this.url + letter;
             }
             console.log(this.url);
+            this.postDraw();
             this.reset();
         });
     }
 
+    postDraw(): void {
+        const draw: Drawing = { _id: this.id, name: this.id, tags: [], url: this.url };
+        this.http.post<Drawing>(this.url, draw);
+    }
+
     reset(): void {
+        this.id = '';
         this.url = '';
     }
 }
