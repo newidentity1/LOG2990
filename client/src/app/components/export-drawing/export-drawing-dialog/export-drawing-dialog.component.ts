c8c3d3add9dffa69d1a24a85c9f01638c02b2035
Estefan Vega-Calcada <estefan.vega-calcada@polymtl.ca>
2020-10-30 20:12:50 -0400

    todo: fix whitebackground, put constants

--- client/src/app/components/export-drawing/export-drawing-dialog/export-drawing-dialog.component.ts
+++ client/src/app/components/export-drawing/export-drawing-dialog/export-drawing-dialog.component.ts
@@ -1,17 +1,6 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DrawingService } from '@app/services/drawing/drawing.service';
-import {
-    CANVAS_MARGIN_LEFT,
-    CANVAS_MARGIN_TOP,
-    CANVAS_MIN_HEIGHT,
-    CANVAS_MIN_WIDTH,
-    DEFAULT_HEIGHT,
-    DEFAULT_WIDTH,
-    PREVIEW_CANVAS_HEIGHT,
-    PREVIEW_CANVAS_WIDTH,
-    SELECTION_CONTROL_POINT_SIZE,
-} from '@app/constants/constants';
 
 @Component({
     selector: 'app-export-drawing-dialog',
@@ -19,13 +8,15 @@ import {
     styleUrls: ['./export-drawing-dialog.component.scss'],
 })
 export class ExportDrawingDialogComponent implements AfterViewInit {
-    @ViewChild('drawingPreviewContainer') drawingPreviewContainer: ElementRef;
-    @ViewChild('drawingPreview') drawingPreview: ElementRef;
-    @ViewChild('cloneCanvas') cloneCanvas: ElementRef<HTMLCanvasElement>;
+    @ViewChild('drawingImageContainer') drawingImageContainer: ElementRef;
+    @ViewChild('drawingImage') drawingImage: ElementRef; // Image of canvas, created from exportCanvas
+    @ViewChild('previewCanvas') previewCanvas: ElementRef<HTMLCanvasElement>; // Visualisation canvas with filters
+    @ViewChild('exportCanvas') exportCanvas: ElementRef<HTMLCanvasElement>; // Exported canvas, copy of real canvas with filters
     selectedFormat: string;
     selectedFilter: string;
     drawingTitle: string;
-    cloneCtx: CanvasRenderingContext2D;
+    previewCtx: CanvasRenderingContext2D;
+    exportCtx: CanvasRenderingContext2D;
 
     constructor(public dialog: MatDialog, public drawingService: DrawingService) {
         this.selectedFormat = 'jpeg';
@@ -35,104 +26,165 @@ export class ExportDrawingDialogComponent implements AfterViewInit {
 
     ngAfterViewInit(): void {
         // Set clone context
-        this.cloneCtx = this.cloneCanvas.nativeElement.getContext('2d') as CanvasRenderingContext2D;
+        this.previewCtx = this.previewCanvas.nativeElement.getContext('2d') as CanvasRenderingContext2D;
+        this.exportCtx = this.exportCanvas.nativeElement.getContext('2d') as CanvasRenderingContext2D;
 
         // Set Preview
         this.setInitialCanvasSize();
-        while (this.cloneCanvas.nativeElement.width > PREVIEW_CANVAS_WIDTH || this.cloneCanvas.nativeElement.height > PREVIEW_CANVAS_HEIGHT) {
-            this.scalePreview();
-        }
-        // this.cloneCtx.globalCompositeOperation = 'destination-over';
+        this.previewCtx.globalCompositeOperation = 'destination-over';
+        // this.whiteBackground();
         this.whiteBackground();
-        // this.cloneCtx.globalCompositeOperation = 'source-over';
+        this.previewCtx.globalCompositeOperation = 'source-over';
 
+        // this.exportCtx.globalCompositeOperation = 'destination-over';
+        // this.whiteBackground();
+        // this.exportCtx.globalCompositeOperation = 'source-over';
+
+        this.setPreviewFilter();
         this.setImageUrl();
     }
 
     setInitialCanvasSize(): void {
-        this.cloneCanvas.nativeElement.width = this.drawingService.canvas.width;
-        this.cloneCanvas.nativeElement.height = this.drawingService.canvas.height;
-    }
-
-    scalePreview(): void {
-        this.cloneCtx.save();
-        this.cloneCtx.translate(50, 50);
-        this.cloneCtx.drawImage(this.cloneCanvas.nativeElement, 0, 0);
-        this.cloneCtx.translate(100, 0);
-        this.cloneCtx.scale(0.75, 0.75);
-        this.cloneCtx.drawImage(this.cloneCanvas.nativeElement, 0, 0);
-
-        this.cloneCtx.translate(133.333, 0);
-        this.cloneCtx.scale(0.75, 0.75);
-        this.cloneCtx.drawImage(this.cloneCanvas.nativeElement, 0, 0);
-        this.cloneCtx.restore();
-        this.cloneCanvas.nativeElement.height /= 2;
-        this.cloneCanvas.nativeElement.width /= 2;
+        this.previewCanvas.nativeElement.width = 800;
+        this.previewCanvas.nativeElement.height = 400;
+        this.exportCanvas.nativeElement.width = this.drawingService.canvas.width;
+        this.exportCanvas.nativeElement.height = this.drawingService.canvas.height;
     }
 
     whiteBackground(): void {
-        this.cloneCtx.fillStyle = '#FFFFFF';
-        this.cloneCtx.fillRect(0, 0, this.cloneCanvas.nativeElement.width, this.cloneCanvas.nativeElement.height);
-        this.cloneCtx.fillStyle = '#000000';
+        this.previewCtx.fillStyle = '#FFFFFF';
+        this.previewCtx.fillRect(0, 0, this.previewCanvas.nativeElement.width, this.previewCanvas.nativeElement.height);
+        this.previewCtx.fillStyle = '#000000';
+    }
+
+    whiteBackground1(ctx: CanvasRenderingContext2D): void {
+        ctx.fillStyle = '#FFFFFF';
+        ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        ctx.fillStyle = '#000000';
+    }
+
+    whiteBackground2(): void {
+        this.exportCtx.fillStyle = '#FFFFFF';
+        this.exportCtx.fillRect(0, 0, this.exportCanvas.nativeElement.width, this.exportCanvas.nativeElement.height);
+        this.exportCtx.fillStyle = '#000000';
     }
 
     onFormatChange(): void {
         this.setImageUrl();
     }
 
-    onFilterChange(): void {
+    drawPreviewCanvas(): void {
+        this.previewCtx.drawImage(
+            this.drawingService.canvas,
+            0,
+            0,
+            this.drawingService.canvas.width,
+            this.drawingService.canvas.height,
+            0,
+            0,
+            800,
+            400,
+        );
+    }
+    // Sets filters on the preview
+    setPreviewFilter(): void {
         switch (this.selectedFilter) {
             case '0':
-                this.cloneCtx.filter = 'none';
+                this.previewCtx.filter = 'none';
                 this.whiteBackground();
-                this.cloneCtx.drawImage(this.drawingService.canvas, 0, 0);
+                // this.whiteBackground1(previewCtx);
+                this.drawPreviewCanvas();
                 break;
             case '1':
-                this.cloneCtx.filter = 'blur(7px)';
+                this.previewCtx.filter = 'blur(7px)';
                 this.whiteBackground();
-                this.cloneCtx.drawImage(this.drawingService.canvas, 0, 0);
-                this.cloneCtx.filter = 'none';
+                // this.whiteBackground1(previewCtx);
+                this.drawPreviewCanvas();
+                this.previewCtx.filter = 'none';
                 break;
             case '2':
-                this.cloneCtx.filter = 'contrast(1.4) sepia(1)';
+                this.previewCtx.filter = 'contrast(1.4) sepia(1)';
                 this.whiteBackground();
-                this.cloneCtx.drawImage(this.drawingService.canvas, 0, 0);
-                this.cloneCtx.filter = 'none';
+                this.drawPreviewCanvas();
+                this.previewCtx.filter = 'none';
                 break;
             case '3':
-                this.cloneCtx.filter = 'brightness(50%)';
+                this.previewCtx.filter = 'brightness(50%)';
                 this.whiteBackground();
-                this.cloneCtx.drawImage(this.drawingService.canvas, 0, 0);
-                this.cloneCtx.filter = 'none';
+                this.drawPreviewCanvas();
+                this.previewCtx.filter = 'none';
                 break;
             case '4':
-                this.cloneCtx.filter = 'saturate(50%)';
+                this.previewCtx.filter = 'saturate(50%)';
                 this.whiteBackground();
-                this.cloneCtx.drawImage(this.drawingService.canvas, 0, 0);
-                this.cloneCtx.filter = 'none';
+                this.drawPreviewCanvas();
+                this.previewCtx.filter = 'none';
                 break;
             case '5':
-                this.cloneCtx.filter = 'dropshadow(50%)';
+                this.previewCtx.filter = 'dropshadow(50%)';
                 this.whiteBackground();
-                this.cloneCtx.drawImage(this.drawingService.canvas, 0, 0);
-                this.cloneCtx.filter = 'none';
+                this.drawPreviewCanvas();
+                this.previewCtx.filter = 'none';
+                break;
+        }
+    }
+
+    // Sets filter on the exported canvas
+    setExportFilter(): void {
+        switch (this.selectedFilter) {
+            case '0':
+                this.exportCtx.filter = 'none';
+                this.whiteBackground2();
+                this.exportCtx.drawImage(this.drawingService.canvas, 0, 0);
+                break;
+            case '1':
+                this.exportCtx.filter = 'blur(7px)';
+                this.whiteBackground2();
+                this.exportCtx.drawImage(this.drawingService.canvas, 0, 0);
+                this.exportCtx.filter = 'none';
+                break;
+            case '2':
+                this.exportCtx.filter = 'contrast(1.4) sepia(1)';
+                this.whiteBackground2();
+                this.exportCtx.drawImage(this.drawingService.canvas, 0, 0);
+                this.exportCtx.filter = 'none';
+                break;
+            case '3':
+                this.exportCtx.filter = 'brightness(50%)';
+                this.whiteBackground2();
+                this.exportCtx.drawImage(this.drawingService.canvas, 0, 0);
+                this.exportCtx.filter = 'none';
+                break;
+            case '4':
+                this.exportCtx.filter = 'saturate(50%)';
+                this.whiteBackground2();
+                this.exportCtx.drawImage(this.drawingService.canvas, 0, 0);
+                this.exportCtx.filter = 'none';
+                break;
+            case '5':
+                this.exportCtx.filter = 'dropshadow(50%)';
+                this.whiteBackground2();
+                this.exportCtx.drawImage(this.drawingService.canvas, 0, 0);
+                this.exportCtx.filter = 'none';
                 break;
         }
     }
 
     downloadImage(): void {
+        // RESET CANVAS BEFORE DOWNLOAD
+        this.setExportFilter();
         this.setImageUrl();
         const title = this.drawingTitle.length > 0 ? this.drawingTitle : 'image';
-        this.drawingPreviewContainer.nativeElement.download = title + '.' + this.selectedFormat;
-        this.drawingPreviewContainer.nativeElement.click();
+        this.drawingImageContainer.nativeElement.download = title + '.' + this.selectedFormat;
+        this.drawingImageContainer.nativeElement.click();
     }
 
     setImageUrl(): void {
         const format = 'image/' + this.selectedFormat;
-        this.onFilterChange();
-        const imageUrl = this.cloneCanvas.nativeElement.toDataURL(format);
+        // this.setExportFilter();
+        const imageUrl = this.exportCanvas.nativeElement.toDataURL(format);
         // const imageUrl = this.drawingService.canvas.toDataURL(format);
-        this.drawingPreview.nativeElement.src = imageUrl;
-        this.drawingPreviewContainer.nativeElement.href = imageUrl;
+        this.drawingImage.nativeElement.src = imageUrl;
+        this.drawingImageContainer.nativeElement.href = imageUrl;
     }
 }
