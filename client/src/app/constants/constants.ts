b4a4e3bf37f8d3004d388daf72678f0b93f4489b
Estefan Vega-Calcada <estefan.vega-calcada@polymtl.ca>
2020-11-02 16:03:32 -0500

    export finished (testing left)

--- client/src/app/constants/constants.ts
+++ client/src/app/constants/constants.ts
@@ -25,9 +25,7 @@ export const MAXIMUM_SIDES = 12;
 
 export const DEFAULT_WIDTH = 1000;
 export const DEFAULT_HEIGHT = 800;
-export const PREVIEW_CANVAS_WIDTH = 800;
-export const PREVIEW_CANVAS_HEIGHT = 400;
-export const MAX_PREVIEW_SIZE = 800;
+export const MAX_PREVIEW_SIZE = 600;
 
 export const CANVAS_MIN_WIDTH = 250;
 export const CANVAS_MIN_HEIGHT = 250;
@@ -63,3 +61,6 @@ export const POURCENTAGE = 100;
 export const BUCKET_PIXEL_3X3 = 3;
 export const INDEX_3 = 3;
 export const TEST_IMAGE_SIZE = 100;
+
+export const BLUR_CONVERSION = 10;
+export const HUE_CONVERSION = 360;
