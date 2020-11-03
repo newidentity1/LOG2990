12a8a5912bc7e67812e2c628512f23c54db3f629
NoeBerguin <berguin.noe@gmail.com>
2020-11-02 19:37:26 -0500

    problem on merge request

--- client/src/app/components/tools-options/polygon/polygon.component.spec.ts
+++ client/src/app/components/tools-options/polygon/polygon.component.spec.ts
@@ -1,9 +1,10 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
-import { MatRadioChange, _MatRadioButtonBase } from '@angular/material/radio';
+import { MatRadioChange, MatRadioModule, _MatRadioButtonBase } from '@angular/material/radio';
 import { MatSlider, MatSliderChange, MatSliderModule } from '@angular/material/slider';
 import { BasicShapeProperties } from '@app/classes/tools-properties/basic-shape-properties';
+import { ThicknessSliderComponent } from '@app/components/tools-options/thickness-slider/thickness-slider.component';
 import { MAXIMUM_SIDES, MAXIMUM_THICKNESS, MINIMUM_SIDES, MINIMUM_THICKNESS } from '@app/constants/constants';
 import { DrawingType } from '@app/enums/drawing-type.enum';
 import { PolygonService } from '@app/services/tools/polygon/polygon.service';
@@ -24,8 +25,9 @@ describe('PolygonComponent', () => {
     beforeEach(async(() => {
         polygonServiceMock = jasmine.createSpyObj('PolygonService', ['setThickness', 'setTypeDrawing', 'setNumberOfSides']);
         TestBed.configureTestingModule({
-            declarations: [PolygonComponent],
-            imports: [MatSliderModule, FormsModule],
+            declarations: [PolygonComponent, ThicknessSliderComponent],
+            imports: [MatSliderModule, MatRadioModule, FormsModule],
+            providers: [{ provide: PolygonService, useValue: polygonServiceMock }],
             schemas: [CUSTOM_ELEMENTS_SCHEMA],
         }).compileComponents();
         polygonServiceMock = TestBed.inject(PolygonService) as jasmine.SpyObj<PolygonService>;
