97e2dcc9dea114f08293022d5def63f08706a5ee
Brando Tovar <grover-brando.tovar-oblitas@polymtl.ca>
2020-11-03 20:17:47 -0500

    fixed selectionType change in selectionService for undo redo

--- client/src/app/classes/tool/shape-tool.ts
+++ client/src/app/classes/tool/shape-tool.ts
@@ -1,7 +1,7 @@
 import { Color } from '@app/classes/color/color';
 import { BasicShapeProperties } from '@app/classes/tools-properties/basic-shape-properties';
 import { Vec2 } from '@app/classes/vec2';
-import { DASHED_SEGMENTS, SELECTION_BOX_THICKNESS, DEFAULT_MITER_LIMIT } from '@app/constants/constants';
+import { DASHED_SEGMENTS, SELECTION_BOX_THICKNESS } from '@app/constants/constants';
 import { DrawingType } from '@app/enums/drawing-type.enum';
 import { MouseButton } from '@app/enums/mouse-button.enum';
 import { DrawingService } from '@app/services/drawing/drawing.service';
