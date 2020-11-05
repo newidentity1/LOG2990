c52ce0b2bf45ca87488ad767435b1e28de29309c
NoeBerguin <berguin.noe@gmail.com>
2020-11-04 19:36:08 -0500

    need to fix test getContex in WarningComponent

--- client/src/app/components/gallery/warning/warning-dialog.component.ts
+++ client/src/app/components/gallery/warning/warning-dialog.component.ts
@@ -10,22 +10,26 @@ import { Drawing } from '@common/communication/drawing';
 })
 export class WarningDialogComponent {
     static drawing: Drawing;
+    private image = new Image();
     constructor(public drawingService: DrawingService, public dialogRef: MatDialogRef<WarningDialogComponent>) {}
 
     deleteCanvas(): void {
-        const image = new Image();
-        image.crossOrigin = '';
-        image.src = WarningDialogComponent.drawing.url;
-        image.onload = () => {
-            const ctx = this.drawingService.canvas.getContext('2d') as CanvasRenderingContext2D;
-            this.drawingService.clearCanvas(ctx as CanvasRenderingContext2D);
-            this.drawingService.baseCtx.canvas.width = image.width;
-            this.drawingService.baseCtx.canvas.height = image.height;
-            this.drawingService.previewCtx.canvas.width = image.width;
-            this.drawingService.previewCtx.canvas.height = image.height;
-            ctx.drawImage(image, 0, 0);
-            this.cancel();
+        this.image.crossOrigin = '';
+        this.image.src = WarningDialogComponent.drawing.url;
+        this.image.onload = () => {
+            this.loadImage();
         };
+    }
+
+    loadImage(): void {
+        const ctx = this.drawingService.canvas.getContext('2d') as CanvasRenderingContext2D;
+        this.drawingService.clearCanvas(ctx as CanvasRenderingContext2D);
+        this.drawingService.baseCtx.canvas.width = this.image.width;
+        this.drawingService.baseCtx.canvas.height = this.image.height;
+        this.drawingService.previewCtx.canvas.width = this.image.width;
+        this.drawingService.previewCtx.canvas.height = this.image.height;
+        ctx.drawImage(this.image, 0, 0);
+        this.cancel();
     }
 
     cancel(): void {
