0d42e0ef710076bbc31795e4ef28cc73585dd674
Yanis Toubal <yanis.toubal@polymtl.ca>
2020-11-09 13:06:46 -0500

    added text component file

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -51,6 +51,7 @@ import { PencilComponent } from './components/tools-options/pencil/pencil.compon
 import { PolygonComponent } from './components/tools-options/polygon/polygon.component';
 import { RectangleComponent } from './components/tools-options/rectangle/rectangle.component';
 import { SelectionComponent } from './components/tools-options/selection/selection.component';
+import { TextComponent } from './components/tools-options/text/text/text.component';
 import { ThicknessSliderComponent } from './components/tools-options/thickness-slider/thickness-slider.component';
 import { UndoRedoComponent } from './components/undo-redo/undo-redo.component';
 import { UploadDialogComponent } from './components/upload/upload-dialog/upload-dialog.component';
@@ -90,6 +91,7 @@ import { UploadComponent } from './components/upload/upload.component';
         UploadComponent,
         UploadDialogComponent,
         WarningDialogComponent,
+        TextComponent,
     ],
     imports: [
         NgImageSliderModule,
---
+++ client/src/app/components/tools-options/text/text.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-text',
+    templateUrl: './text.component.html',
+    styleUrls: ['../../sidebar/sidebar.component.scss'],
+})
+export class TextComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
