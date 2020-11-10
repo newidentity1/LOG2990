715aaf2cf26ce162ec1f6ae8a95d74327fd74de0
Yanis Toubal <yanis.toubal@polymtl.ca>
2020-11-09 22:04:16 -0500

    text now works

--- client/src/app/services/shortcut/shortcut.service.ts
+++ client/src/app/services/shortcut/shortcut.service.ts
@@ -1,19 +1,25 @@
 import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { EventManager } from '@angular/platform-browser';
+import { ToolbarService } from '@app/services/toolbar/toolbar.service';
+import { TextService } from '@app/services/tools/text/text.service';
 import { Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
 })
 export class ShortcutService {
-    constructor(private eventManager: EventManager, private dialog: MatDialog) {}
+    // TODO voir avoir chargé
+    // tslint:disable-next-line: ban-types
+    globalListener: Function | undefined;
+    constructor(private eventManager: EventManager, private dialog: MatDialog, private toolbarService: ToolbarService) {}
 
     addShortcut(keys: string): Observable<KeyboardEvent> {
         return new Observable((observer) => {
             const handler = (event: KeyboardEvent) => {
+                const isWritingTextMode = this.toolbarService.currentTool instanceof TextService && this.toolbarService.currentTool.mouseDown;
                 // disable shortcuts if a modal is open
-                if (this.dialog.openDialogs.length === 0) {
+                if (this.dialog.openDialogs.length === 0 && !isWritingTextMode) {
                     event.preventDefault();
                     observer.next(event);
                 }
@@ -21,5 +27,19 @@ export class ShortcutService {
 
             this.eventManager.addGlobalEventListener('document', `keydown.${keys}`, handler);
         });
+    }
+
+    writingTextMode(): void {
+        this.globalListener = this.eventManager.addGlobalEventListener(
+            'document',
+            'keydown',
+            this.toolbarService.currentTool.onKeyDown.bind(this.toolbarService.currentTool),
+        );
+    }
+
+    removeWritingTextMode(): void {
+        if (this.globalListener) {
+            this.globalListener();
+        }
     }
 }
