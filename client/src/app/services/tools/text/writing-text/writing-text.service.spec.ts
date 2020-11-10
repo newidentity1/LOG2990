251a478295c12319d1d39cf2f26a773a64ed5757
Yanis Toubal <yanis.toubal@polymtl.ca>
2020-11-09 23:10:10 -0500

    fixed interaction of shortcuts and text

---
+++ client/src/app/services/tools/text/writing-text/writing-text.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { WritingTextService } from './writing-text.service';
+
+describe('WritingTextService', () => {
+  let service: WritingTextService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(WritingTextService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
