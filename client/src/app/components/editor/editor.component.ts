7b871c4aef1e151dd50a885e0771f559ed310210
Yanis Toubal <yanis.toubal@polymtl.ca>
2020-11-13 16:40:54 -0500

    refactored onKeyDown and added backspace handler for text service

--- client/src/app/components/editor/editor.component.ts
+++ client/src/app/components/editor/editor.component.ts
@@ -41,6 +41,11 @@ export class EditorComponent implements OnInit, AfterViewInit, OnDestroy {
         });
     }
 
+    @HostListener('window:keydown', ['$event'])
+    onKeyDown(event: KeyboardEvent): void {
+        this.toolbarService.onKeyDown(event);
+    }
+
     @HostListener('window:keyup', ['$event'])
     onKeyUp(event: KeyboardEvent): void {
         event.preventDefault();
@@ -120,14 +125,5 @@ export class EditorComponent implements OnInit, AfterViewInit, OnDestroy {
                 this.toolbarService.redo();
             }),
         );
-
-        const keyDownButtons = ['Escape', 'Shift', 'Backspace', 'ArrowLeft', 'ArrowUp', 'ArrowRight', 'ArrowDown'];
-        keyDownButtons.forEach((button: string) => {
-            this.subscribedShortcuts.push(
-                this.shortcutService.addShortcut(button).subscribe((event: KeyboardEvent) => {
-                    this.toolbarService.onKeyDown(event);
-                }),
-            );
-        });
     }
 }
