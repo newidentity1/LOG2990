50d2c06f37d3faef982dc1259a97a210bf703b98
Yanis Toubal <yanis.toubal@polymtl.ca>
2020-11-22 22:45:15 -0500

    added stam properties and shortcut

--- client/src/app/services/tools/stamp/stamp.service.ts
+++ client/src/app/services/tools/stamp/stamp.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Tool } from '@app/classes/tool/tool';
+import { StampProperties } from '@app/classes/tools-properties/stamp-properties';
+import { DrawingService } from '@app/services/drawing/drawing.service';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
-export class StampService {
-
-  constructor() { }
+export class StampService extends Tool {
+    constructor(drawingService: DrawingService) {
+        super(drawingService);
+        this.name = 'Stamp';
+        this.tooltip = 'Étampe(d)';
+        this.iconName = 'insert_emoticon';
+        this.toolProperties = new StampProperties();
+    }
 }
