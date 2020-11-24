02d3591070a3a137a94d07f25388880f939d7417
Yanis Toubal <yanis.toubal@polymtl.ca>
2020-11-24 12:14:14 -0500

    refactor structure of the code of resize canvas

---
+++ client/src/app/enums/control-point.enum.ts
@@ -0,0 +1,10 @@
+export enum ControlPoint {
+    TopLeft = 0,
+    TopCenter = 1,
+    TopRight = 2,
+    CenterLeft = 3,
+    CenterRight = 4,
+    BottomLeft = 5,
+    BottomCenter = 6,
+    BottomRight = 7,
+}
