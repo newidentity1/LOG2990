a32ff32af9c308ffde433fd73bf5e7edb907d433
Yanis Toubal <yanis.toubal@polymtl.ca>
2020-11-25 23:31:28 -0500

    image scaling works with resize on preview

--- client/src/app/services/tools/selection/resize-selection/resize-selection.service.ts
+++ client/src/app/services/tools/selection/resize-selection/resize-selection.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Renderer2, RendererFactory2 } from '@angular/core';
 import { Vec2 } from '@app/classes/vec2';
 import { ControlPoint } from '@app/enums/control-point.enum';
 import { DrawingService } from '@app/services/drawing/drawing.service';
@@ -8,9 +8,14 @@ import { ResizeService } from '@app/services/resize/resize.service';
     providedIn: 'root',
 })
 export class ResizeSelectionService {
-    startingPoint: Vec2;
+    private startingPoint: Vec2;
+    private renderer: Renderer2;
+    private selectionImageCanvas: HTMLCanvasElement;
 
-    constructor(private resizeService: ResizeService, private drawingService: DrawingService) {}
+    constructor(private resizeService: ResizeService, private drawingService: DrawingService, private rendererFactory: RendererFactory2) {
+        this.renderer = this.rendererFactory.createRenderer(null, null);
+        this.selectionImageCanvas = this.renderer.createElement('canvas');
+    }
 
     onResize(event: MouseEvent, startingPoint: Vec2): Vec2 {
         this.startingPoint = startingPoint;
@@ -72,6 +77,27 @@ export class ResizeSelectionService {
         if (!isControlTopSide) return;
         this.startingPoint.y = event.clientY - this.drawingService.canvas.getBoundingClientRect().y;
         this.drawingService.previewCtx.canvas.style.top = this.startingPoint.y + 'px';
+    }
+
+    scaleImage(selectionImage: ImageData): ImageData {
+        console.log('scale');
+        const selectionWidth = this.drawingService.previewCtx.canvas.width;
+        const selectionHeight = this.drawingService.previewCtx.canvas.height;
+
+        const scaleX = selectionWidth / selectionImage.width;
+        const scaleY = selectionHeight / selectionImage.height;
+
+        this.selectionImageCanvas.width = selectionImage.width;
+        this.selectionImageCanvas.height = selectionImage.height;
+        const context = this.selectionImageCanvas.getContext('2d') as CanvasRenderingContext2D;
+        context.putImageData(selectionImage, 0, 0);
+
+        this.drawingService.clearCanvas(this.drawingService.previewCtx);
+        this.drawingService.previewCtx.scale(scaleX, scaleY);
+        this.drawingService.previewCtx.drawImage(this.selectionImageCanvas, 0, 0);
+        this.drawingService.previewCtx.scale(1, 1);
+
+        return this.drawingService.previewCtx.getImageData(0, 0, selectionWidth, selectionHeight);
     }
 
     get isResizing(): boolean {
