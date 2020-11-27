9ad59b57ab3a8efe998d52aa53d98eb0144b1948
Yanis Toubal <yanis.toubal@polymtl.ca>
2020-11-26 23:21:36 -0500

    added mirror for selection

--- client/src/app/services/tools/selection/resize-selection/resize-selection.service.ts
+++ client/src/app/services/tools/selection/resize-selection/resize-selection.service.ts
@@ -11,10 +11,21 @@ export class ResizeSelectionService {
     private startingPoint: Vec2;
     private renderer: Renderer2;
     private selectionImageCanvas: HTMLCanvasElement;
+    private oppositeControlPoints: Map<ControlPoint, ControlPoint> = new Map<ControlPoint, ControlPoint>();
+    private oppositeTwoAxesDone: boolean = false;
 
     constructor(private resizeService: ResizeService, private drawingService: DrawingService, private rendererFactory: RendererFactory2) {
         this.renderer = this.rendererFactory.createRenderer(null, null);
         this.selectionImageCanvas = this.renderer.createElement('canvas');
+        this.oppositeControlPoints
+            .set(ControlPoint.BottomCenter, ControlPoint.TopCenter)
+            .set(ControlPoint.BottomLeft, ControlPoint.TopRight)
+            .set(ControlPoint.BottomRight, ControlPoint.TopLeft)
+            .set(ControlPoint.CenterLeft, ControlPoint.CenterRight)
+            .set(ControlPoint.CenterRight, ControlPoint.CenterLeft)
+            .set(ControlPoint.TopCenter, ControlPoint.BottomCenter)
+            .set(ControlPoint.TopRight, ControlPoint.BottomLeft)
+            .set(ControlPoint.TopLeft, ControlPoint.BottomRight);
     }
 
     onResize(event: MouseEvent, startingPoint: Vec2): Vec2 {
@@ -22,6 +33,7 @@ export class ResizeSelectionService {
         this.onResizeWidth(event);
         this.onResizeHeight(event);
         this.onChangeStartingPoint(event);
+        this.oppositeTwoAxesDone = false;
         return this.startingPoint;
     }
 
@@ -35,7 +47,13 @@ export class ResizeSelectionService {
         if (isHeightOnlyControlPoint) return;
         const widthDiff = event.clientX - this.drawingService.canvas.getBoundingClientRect().x - this.startingPoint.x;
         const oldWidth = this.drawingService.previewCtx.canvas.width;
-        const newWidth = isControlLeftSide ? oldWidth - widthDiff : widthDiff;
+        let newWidth = isControlLeftSide ? oldWidth - widthDiff : widthDiff;
+
+        if (newWidth <= 0) {
+            newWidth = newWidth === 0 ? 1 : newWidth;
+            if (!this.oppositeTwoAxesDone) this.changeOppositeControlPoint();
+        }
+
         this.drawingService.previewCtx.canvas.width = newWidth;
     }
 
@@ -50,7 +68,13 @@ export class ResizeSelectionService {
         if (isHeightOnlyControlPoint) return;
         const heightDiff = event.clientY - this.drawingService.canvas.getBoundingClientRect().y - this.startingPoint.y;
         const oldHeight = this.drawingService.previewCtx.canvas.height;
-        const newHeight = isControlTopSide ? oldHeight - heightDiff : heightDiff;
+        let newHeight = isControlTopSide ? oldHeight - heightDiff : heightDiff;
+
+        if (newHeight <= 0) {
+            newHeight = newHeight === 0 ? 1 : newHeight;
+            if (!this.oppositeTwoAxesDone) this.changeOppositeControlPoint();
+        }
+
         this.drawingService.previewCtx.canvas.height = newHeight;
     }
 
@@ -96,6 +120,22 @@ export class ResizeSelectionService {
         this.drawingService.previewCtx.drawImage(this.selectionImageCanvas, 0, 0, selectionWidth, selectionHeight);
 
         return this.drawingService.previewCtx.getImageData(0, 0, selectionWidth, selectionHeight);
+    }
+
+    changeOppositeControlPoint(): void {
+        if (this.resizeService.controlPoint === null) return;
+        const oppositeControlPoint = this.oppositeControlPoints.get(this.resizeService.controlPoint);
+        this.resizeService.controlPoint = oppositeControlPoint !== undefined ? oppositeControlPoint : this.resizeService.controlPoint;
+        console.log(this.resizeService.controlPoint, oppositeControlPoint);
+        if (
+            (oppositeControlPoint === ControlPoint.BottomLeft ||
+                oppositeControlPoint === ControlPoint.BottomRight ||
+                oppositeControlPoint === ControlPoint.TopLeft ||
+                oppositeControlPoint === ControlPoint.TopRight) &&
+            this.resizeService.controlPoint === oppositeControlPoint
+        ) {
+            this.oppositeTwoAxesDone = true;
+        }
     }
 
     get isResizing(): boolean {
