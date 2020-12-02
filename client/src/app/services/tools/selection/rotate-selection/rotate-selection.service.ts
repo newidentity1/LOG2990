e8e0907da88ffef3995e6a838ecda392d328b930
Brando Tovar <grover-brando.tovar-oblitas@polymtl.ca>
2020-12-01 19:52:52 -0500

    fixed bugg rotate selection

--- client/src/app/services/tools/selection/rotate-selection/rotate-selection.service.ts
+++ client/src/app/services/tools/selection/rotate-selection/rotate-selection.service.ts
@@ -8,26 +8,32 @@ import { DrawingService } from '@app/services/drawing/drawing.service';
 export class RotateSelectionService {
     angle: number = 0;
     tempCtx: CanvasRenderingContext2D;
+    imgDataUrl: string = '';
 
     constructor(private drawingService: DrawingService) {}
 
     scroll(event: WheelEvent, selectionImageData: ImageData): void {
+        this.angle += (Math.sign(event.deltaY) * (DEFAULT_ROTATION_ANGLE * Math.PI)) / STRAIGHT_ANGLE;
+        this.rotateImage(selectionImageData);
+    }
+
+    rotateImage(image: ImageData): void {
         this.tempCtx = this.drawingService.canvas.getContext('2d') as CanvasRenderingContext2D;
         const tempCanvas = this.tempCtx.canvas;
         const canvas = this.drawingService.previewCtx.canvas;
         const ctx = this.drawingService.previewCtx;
 
-        this.tempCtx.putImageData(selectionImageData, 0, 0);
+        this.tempCtx.putImageData(image, 0, 0);
         this.drawingService.clearCanvas(ctx);
 
         ctx.save();
-        this.angle += (Math.sign(event.deltaY) * (DEFAULT_ROTATION_ANGLE * Math.PI)) / STRAIGHT_ANGLE;
         ctx.beginPath();
         ctx.translate(canvas.width / 2, canvas.height / 2);
         ctx.rotate(this.angle);
         ctx.translate(-canvas.width / 2, -canvas.height / 2);
         ctx.drawImage(tempCanvas, 0, 0);
         ctx.restore();
-        this.tempCtx.drawImage(ctx.canvas, 0, 0);
+
+        this.drawingService.clearCanvas(this.tempCtx);
     }
 }
