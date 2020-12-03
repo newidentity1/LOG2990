5bae2b71696984becf7b089d0dbad893bc8ed5fa
momoislam <mohammed-ariful.islam@polymtl.ca>
2020-12-02 19:10:42 -0500

    added calligraphy  line length and angle change

--- client/src/app/services/tools/calligraphy/calligraphy.service.ts
+++ client/src/app/services/tools/calligraphy/calligraphy.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BrushProperties } from '@app/classes/tools-properties/brush-properties';
+import { Vec2 } from '@app/classes/vec2';
 import { DrawingService } from '@app/services/drawing/drawing.service';
 import { PencilService } from '@app/services/tools/pencil/pencil-service';
 
@@ -7,6 +8,9 @@ import { PencilService } from '@app/services/tools/pencil/pencil-service';
     providedIn: 'root',
 })
 export class CalligraphyService extends PencilService {
+    lineLength: number = 10; // TODO: use constants
+    lineAngle: number = 0;
+
     constructor(drawingService: DrawingService) {
         super(drawingService);
         this.name = 'Calligraphy';
@@ -14,5 +18,37 @@ export class CalligraphyService extends PencilService {
         this.iconName = 'history_edu';
         this.toolProperties = new BrushProperties();
         this.clearPath();
+    }
+
+    draw(ctx: CanvasRenderingContext2D): void {
+        console.log(this.lineAngle);
+        ctx.lineWidth = 2;
+        for (const point of this.pathData) {
+            ctx.beginPath();
+            ctx.lineTo(
+                point.x - (this.lineLength / 2) * Math.cos((this.lineAngle * Math.PI) / 180),
+                point.y - (this.lineLength / 2) * Math.sin((this.lineAngle * Math.PI) / 180),
+            );
+            ctx.lineTo(
+                point.x + (this.lineLength / 2) * Math.cos((this.lineAngle * Math.PI) / 180),
+                point.y + (this.lineLength / 2) * Math.sin((this.lineAngle * Math.PI) / 180),
+            );
+            ctx.stroke();
+        }
+    }
+
+    protected drawCursor(position: Vec2): void {
+        const cursorCtx = this.drawingService.previewCtx;
+        this.drawingService.clearCanvas(cursorCtx);
+        cursorCtx.beginPath();
+        cursorCtx.lineTo(
+            position.x - (this.lineLength / 2) * Math.cos((this.lineAngle * Math.PI) / 180),
+            position.y - (this.lineLength / 2) * Math.sin((this.lineAngle * Math.PI) / 180),
+        );
+        cursorCtx.lineTo(
+            position.x + (this.lineLength / 2) * Math.cos((this.lineAngle * Math.PI) / 180),
+            position.y + (this.lineLength / 2) * Math.sin((this.lineAngle * Math.PI) / 180),
+        );
+        cursorCtx.stroke();
     }
 }
