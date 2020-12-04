ce7c219028d11170cc9b32c7c2cfd9dbffa95cca
Noe <noe.berguin@polymtl.ca>
2020-12-03 19:33:35 -0500

    stickers background is now transparent ans cursor is hide

--- client/src/app/services/tools/stamp/stamp.service.ts
+++ client/src/app/services/tools/stamp/stamp.service.ts
@@ -9,7 +9,6 @@ import { DrawingService } from '@app/services/drawing/drawing.service';
 })
 export class StampService extends Tool {
     src: string = '../../../assets/stamp/1.png';
-    imageData: ImageData;
     finalPosition: Vec2 = { x: 0, y: 0 };
     constructor(drawingService: DrawingService) {
         super(drawingService);
@@ -17,71 +16,25 @@ export class StampService extends Tool {
         this.tooltip = 'Étampe(d)';
         this.iconName = 'insert_emoticon';
         this.toolProperties = new StampProperties();
-        this.src = '../../../assets/stamp/1.png';
+        this.src = '';
     }
 
     onClick(event: MouseEvent): void {
-        console.log('on est ici');
-        this.drawingService.baseCtx.putImageData(
-            this.imageData,
-            this.finalPosition.x,
-            this.finalPosition.y,
-            0,
-            0,
-            this.imageData.width,
-            this.imageData.height,
-        );
-    }
-
-    getImageData(): void {
         const image = new Image();
         image.crossOrigin = '';
         image.src = this.src;
-        image.onload = () => {
-            this.drawingService.previewCtx.canvas.width = image.width;
-            this.drawingService.previewCtx.canvas.height = image.height;
-            this.drawingService.previewCtx.drawImage(image, 0, 0);
-            this.imageData = this.drawingService.previewCtx.getImageData(0, 0, image.width, image.height);
-            // this.drawingService.clearCanvas(this.drawingService.previewCtx);
-        };
+        this.drawingService.baseCtx.drawImage(image, this.finalPosition.x, this.finalPosition.y);
     }
 
     onMouseMove(event: MouseEvent): void {
-        const x = event.clientX - this.drawingService.baseCtx.canvas.getBoundingClientRect().x;
-        const y = event.clientY - this.drawingService.baseCtx.canvas.getBoundingClientRect().y;
-        this.moveSelection(x, y);
-    }
-
-    moveSelection(moveX: number, moveY: number): void {
-        this.getImageData();
-        this.finalPosition.x = moveX - this.imageData.width / 2;
-        this.finalPosition.y = moveY - this.imageData.height / 2;
-
-        this.drawingService.previewCtx.canvas.style.left = moveX - this.imageData.width / 2 + 'px';
-        this.drawingService.previewCtx.canvas.style.top = moveY - this.imageData.height / 2 + 'px';
-
-        this.drawingService.clearCanvas(this.drawingService.previewCtx);
-        this.drawingService.previewCtx.putImageData(
-            this.imageData,
-            0,
-            0,
-            this.finalPosition.x >= 0 ? 0 : -this.finalPosition.x,
-            this.finalPosition.y >= 0 ? 0 : -this.finalPosition.y,
-            this.drawingService.canvas.width - this.finalPosition.x,
-            this.drawingService.canvas.height - this.finalPosition.y,
-        );
-        const selectionCtx = this.drawingService.previewCtx;
-        selectionCtx.canvas.style.cursor = 'none';
-    }
-
-    resetContext(): void {
-        console.log('Reset');
-        const previewCtx = this.drawingService.previewCtx;
-        const baseCtx = this.drawingService.baseCtx;
-        previewCtx.lineCap = baseCtx.lineCap = 'butt';
-        previewCtx.lineJoin = baseCtx.lineJoin = 'miter';
-        this.mouseDown = false;
-        this.drawingService.clearCanvas(this.drawingService.previewCtx);
-        this.setThickness(this.toolProperties.thickness);
+        const image = new Image();
+        image.crossOrigin = '';
+        image.src = this.src;
+        this.finalPosition.x = event.clientX - this.drawingService.baseCtx.canvas.getBoundingClientRect().x - image.width / 2;
+        this.finalPosition.y = event.clientY - this.drawingService.baseCtx.canvas.getBoundingClientRect().y - image.height / 2;
+        const cursorCtx = this.drawingService.previewCtx;
+        this.drawingService.clearCanvas(cursorCtx);
+        this.drawingService.previewCtx.drawImage(image, this.finalPosition.x, this.finalPosition.y);
+        this.drawingService.previewCtx.canvas.style.cursor = 'none';
     }
 }
