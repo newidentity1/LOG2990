0028f84d82ca27067b20f4561ebea868f2e68e60
Yanis Toubal <yanis.toubal@polymtl.ca>
2020-12-04 14:43:49 -0500

    fixed cursor bug in text service

--- client/src/app/classes/tools-properties/text-properties.ts
+++ client/src/app/classes/tools-properties/text-properties.ts
@@ -4,7 +4,7 @@ import { BasicToolProperties } from './basic-tool-properties';
 export class TextProperties extends BasicToolProperties {
     isBold: boolean = false;
     isItalic: boolean = false;
-    fonts: string[] = [TextFont.Arial, TextFont.Verdana, TextFont.TimesNewRoman, TextFont.Georgia, TextFont.CourierNew, TextFont.LucidaConsole];
+    fonts: string[] = [TextFont.Arial, TextFont.Verdana, TextFont.TimesNewRoman, TextFont.Georgia, TextFont.CourierNew];
     font: string = TextFont.Arial;
     size: number = 20;
     textAlignments: string[] = [TextAlignment.Left, TextAlignment.Middle, TextAlignment.Right];
