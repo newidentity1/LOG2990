6ab16f12e33f191afff7e927e08e97d080659454
momoislam <mohammed-ariful.islam@polymtl.ca>
2020-12-06 20:59:50 -0500

    fix lint issues

--- client/src/app/services/tools/selection/move-selection/move-selection.service.ts
+++ client/src/app/services/tools/selection/move-selection/move-selection.service.ts
@@ -98,8 +98,8 @@ export class MoveSelectionService {
 
         for (let i = 0; i < this.imgData.data.length; i += CONSTANTS.IMAGE_DATA_OPACITY_INDEX + 1) {
             if (currentType === SelectionType.EllipseSelection) {
-                const x = (i / 4) % this.imgData.width;
-                const y = Math.floor(i / 4 / this.imgData.width);
+                const x = (i / (CONSTANTS.IMAGE_DATA_OPACITY_INDEX + 1)) % this.imgData.width;
+                const y = Math.floor(i / (CONSTANTS.IMAGE_DATA_OPACITY_INDEX + 1) / this.imgData.width);
 
                 if (!this.isPositionInEllipse({ x, y }, width, height)) {
                     this.imgData.data[i] = 0;
