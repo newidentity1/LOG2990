c7e24ce09a4fe85b8beebc5c63fc67e3b7d17347
Noe <noe.berguin@polymtl.ca>
2020-12-08 13:58:41 -0500

    add some resize methode for stamp

--- client/src/app/services/tools/stamp/stamp.service.ts
+++ client/src/app/services/tools/stamp/stamp.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Tool } from '@app/classes/tool/tool';
 import { StampProperties } from '@app/classes/tools-properties/stamp-properties';
 import { Vec2 } from '@app/classes/vec2';
+import { DEFAULT_ROTATION_ANGLE, MAXIMUM_ROTATION_ANGLE } from '@app/constants/constants';
 import { DrawingService } from '@app/services/drawing/drawing.service';
 
 @Injectable({
@@ -10,13 +11,16 @@ import { DrawingService } from '@app/services/drawing/drawing.service';
 export class StampService extends Tool {
     src: string = '../../../assets/stamp/1.png';
     finalPosition: Vec2 = { x: 0, y: 0 };
+    uploadedImage: File;
+    size: number = 100;
+    private angle: number = 144;
+    private atlDown: boolean = false;
     constructor(drawingService: DrawingService) {
         super(drawingService);
         this.name = 'Stamp';
         this.tooltip = 'Étampe(d)';
         this.iconName = 'insert_emoticon';
         this.toolProperties = new StampProperties();
-        this.src = '';
     }
 
     onClick(event: MouseEvent): void {
@@ -30,11 +34,32 @@ export class StampService extends Tool {
         const image = new Image();
         image.crossOrigin = '';
         image.src = this.src;
-        this.finalPosition.x = event.clientX - this.drawingService.baseCtx.canvas.getBoundingClientRect().x - image.width / 2;
-        this.finalPosition.y = event.clientY - this.drawingService.baseCtx.canvas.getBoundingClientRect().y - image.height / 2;
+
+        // image.onload = () => {
+        //     image.style.transform = `rotate(${this.angle}deg)`;
+        // };
+
+        this.finalPosition.x = event.x - this.drawingService.baseCtx.canvas.getBoundingClientRect().x - image.width / 2;
+        this.finalPosition.y = event.y - this.drawingService.baseCtx.canvas.getBoundingClientRect().y - image.height / 2;
         const cursorCtx = this.drawingService.previewCtx;
         this.drawingService.clearCanvas(cursorCtx);
         this.drawingService.previewCtx.drawImage(image, this.finalPosition.x, this.finalPosition.y);
         this.drawingService.previewCtx.canvas.style.cursor = 'none';
+    }
+
+    onMouseScroll(event: WheelEvent): void {
+        const previewCtx = this.drawingService.previewCtx.canvas.getContext('2d');
+        this.angle = (this.angle + Math.sign(event.deltaY) * (this.atlDown ? 1 : DEFAULT_ROTATION_ANGLE)) % MAXIMUM_ROTATION_ANGLE;
+        if (this.angle < 0) this.angle = MAXIMUM_ROTATION_ANGLE + this.angle;
+        previewCtx?.rotate(this.angle);
+    }
+
+    onKeyDown(event: KeyboardEvent): void {
+        this.atlDown = event.key === 'Alt' ? true : this.atlDown;
+    }
+
+    onKeyUp(event: KeyboardEvent): void {
+        // TODO: verifier bug avec ALT+TAB
+        this.atlDown = event.key === 'Alt' ? false : this.atlDown;
     }
 }
