8e807ead43b7a8368adf9b2224d9d9f77a7af29b
Yanis Toubal <yanis.toubal@polymtl.ca>
2020-12-09 13:29:23 -0500

    added email api infos in env

--- server/app/constants.ts
+++ server/app/constants.ts
@@ -7,6 +7,10 @@ export const DATABASE_URL: string = process.env.DATABASE_URL as string;
 export const DATABASE_NAME: string = process.env.DATABASE_NAME as string;
 export const DATABASE_COLLECTION: string = process.env.DATABASE_COLLECTION as string;
 
+// Email api infos
+export const EMAIL_API_URL: string = process.env.EMAIL_API_URL as string;
+export const EMAIL_API_KEY: string = process.env.EMAIL_API_KEY as string;
+
 // HTTP status
 export const HTTP_STATUS_OK = 200;
 export const HTTP_STATUS_CREATED = 201;
