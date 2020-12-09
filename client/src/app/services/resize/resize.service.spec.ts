6ea092055bfde62998813962ed7928646b4c7dd9
Brando Tovar <grover-brando.tovar-oblitas@polymtl.ca>
2020-12-08 20:12:22 -0500

    added first test of resize service

--- client/src/app/services/resize/resize.service.spec.ts
+++ client/src/app/services/resize/resize.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
-
-import { ResizeService } from './resize.service';
+import { canvasTestHelper } from '@app/classes/canvas-test-helper';
+import { ResizeService } from '@app/services/resize/resize.service';
 
 describe('ResizeService', () => {
     let service: ResizeService;
@@ -8,9 +8,28 @@ describe('ResizeService', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({});
         service = TestBed.inject(ResizeService);
+        service['drawingService'].canvas = canvasTestHelper.canvas;
+        service['drawingService'].baseCtx = canvasTestHelper.canvas.getContext('2d') as CanvasRenderingContext2D;
+        service['drawingService'].previewCtx = canvasTestHelper.drawCanvas.getContext('2d') as CanvasRenderingContext2D;
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it('resize should should call execute', (done) => {
+        const executeSpy = spyOn(service, 'execute');
+        const drawImageSpy = spyOn(service, 'drawImage');
+        const emitSpy = spyOn(service.executedCommand, 'emit');
+        const value = 10;
+        service.resize(value, value);
+
+        setTimeout(() => {
+            expect(executeSpy).toHaveBeenCalled();
+            expect(drawImageSpy).toHaveBeenCalled();
+            expect(emitSpy).toHaveBeenCalled();
+            done();
+            // tslint:disable-next-line: no-magic-numbers / reason: waiting for image to load
+        }, 200);
     });
 });
