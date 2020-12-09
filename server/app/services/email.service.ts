c1d9accef9d250490c9d3fbb50c02c47bf2a6ef4
Yanis Toubal <yanis.toubal@polymtl.ca>
2020-12-09 13:29:23 -0500

    added email api infos in env

--- server/app/services/email.service.ts
+++ server/app/services/email.service.ts
@@ -1,3 +1,4 @@
+import { EMAIL_API_KEY, EMAIL_API_URL } from '@app/constants';
 import axios from 'axios';
 import * as FormData from 'form-data';
 import { ReadStream } from 'fs';
@@ -6,9 +7,6 @@ import 'reflect-metadata';
 
 @injectable()
 export class EmailService {
-    apiKey: string = 'f6cd41ef-636d-45ae-9e07-47dd97cff25e';
-    apiUrl: string = 'http://log2990.step.polymtl.ca/email';
-
     async sendEmail(to: string, payload: ReadStream, filename: string): Promise<number> {
         if (!this.validateRequestEmail(to)) throw new Error("L'adresse courriel entrée est invalide");
         const bodyFormData = new FormData();
@@ -17,10 +15,10 @@ export class EmailService {
 
         return await axios({
             method: 'post',
-            url: this.apiUrl,
+            url: EMAIL_API_URL,
             data: bodyFormData,
             params: { address_validation: true, quick_return: false, dry_run: false },
-            headers: { 'Content-Type': 'multipart/form-data', 'X-Team-Key': this.apiKey, ...bodyFormData.getHeaders() },
+            headers: { 'Content-Type': 'multipart/form-data', 'X-Team-Key': EMAIL_API_KEY, ...bodyFormData.getHeaders() },
         })
             .then((response) => {
                 // handle success
